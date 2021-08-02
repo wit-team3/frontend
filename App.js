01f298ac68aafc27fd979f3e4b569bd004f54d1b
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-08-02 21:39:35 +0900

    Create a new cli app
    프로젝트를 세팅하여 cli app을 생성합니다.

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>wit-2th-3team-project~!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
